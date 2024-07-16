aea2dc016630350367e7fe7f40bd609d0af35ee0
arthur3000 <arthur3000.tw@gmail.com>
2024-07-16 14:55:34 +0800

    Modify geWeatherWeek.py

--- model/getWeatherWeek.py
+++ model/getWeatherWeek.py
@@ -1,2 +1,18 @@
+import urllib.request as req
+from urllib.parse import quote
+import json
+
 def getWeatherWeek(location):
-    return
+
+    CWB_API_KEY = "CWA-A84BBFA9-3657-4814-BB70-63523EA7AA40"
+
+    url = "https://opendata.cwa.gov.tw/api/v1/rest/datastore/F-D0047-091?Authorization=" + CWB_API_KEY + "&locationName=" + quote(location)
+
+    request = req.Request(url)
+
+    with req.urlopen(request) as response:
+        response_body = response.read().decode('utf-8')
+
+    my_data = json.loads(response_body)
+
+    return my_data
