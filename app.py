b182883a5d683f6216685c6ad198b6f998bd53fb
arthur3000 <arthur3000.tw@gmail.com>
2024-07-16 13:22:18 +0800

    Add getWeatherWeek.py and /weather/week path

--- app.py
+++ app.py
@@ -1,6 +1,7 @@
 from fastapi import FastAPI
 from fastapi.staticfiles import StaticFiles
 from fastapi.responses import FileResponse
+from model.getWeatherWeek import getWeatherWeek
 
 app = FastAPI()
 
@@ -9,3 +10,8 @@ app.mount('/static', StaticFiles(directory='static', html=True))
 @app.get("/")
 async def index():
     return FileResponse("./static/index.html", media_type="text/html")
+
+@app.get("/weather/week")
+async def get_weather_week():
+    result = getWeatherWeek()
+    return result
