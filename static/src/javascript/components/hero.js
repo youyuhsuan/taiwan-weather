9004cdbc510fae1de50d1dcfe9114d3a91e47817
ZChingg <zchingchaoo@gmail.com>
2024-07-18 14:33:22 +0800

    fix: forecast weather background image

--- static/src/javascript/components/hero.js
+++ static/src/javascript/components/hero.js
@@ -4,11 +4,44 @@ async function getHeroData(ctyName = "台北") {
     if (response) {
       let responseData = await response.json();
       let todayWeather = responseData[0].weatherElement;
+      let nowWxNum = todayWeather[1].time[0].elementValue[1].value;
       let nowPoP12h = todayWeather[0].time[0].elementValue[0].value;
       let nowWX = todayWeather[1].time[0].elementValue[0].value;
       let nowT = todayWeather[3].time[0].elementValue[0].value;
       let nowWD = todayWeather[9].time[0].elementValue[0].value;
 
+      // 根據天氣判斷背景圖
+      let BackgroundImageName = "";
+      function getBackgroundImage(BackgroundImageName) {
+        let weatherImage = document.querySelector(".hero-section");
+        let currentBgImage =
+          window.getComputedStyle(weatherImage).backgroundImage;
+        let newBgImage = currentBgImage.replace(
+          /url\([^)]*\)/,
+          `url(/static/src/image/${BackgroundImageName}.jpg)`
+        );
+        weatherImage.style.backgroundImage = newBgImage;
+      }
+
+      if (/01/.test(nowWxNum)) {
+        // 晴
+        BackgroundImageName = "01_clear";
+      } else if (/02|03/.test(nowWxNum)) {
+        // 晴時多雲
+        BackgroundImageName = "05_cloudy";
+      } else if (/0[4-7]|2[4-8]/.test(nowWxNum)) {
+        // 多雲、陰、霧
+        BackgroundImageName = "05_cloudy";
+      } else if (/0[8-9]|1[0-9]|2[0-2]|29|3[0-9]|41/.test(nowWxNum)) {
+        // 雨
+        BackgroundImageName = "rain";
+      } else if (/23|42/.test(nowWxNum)) {
+        // 雪
+        BackgroundImageName = "15_snowing";
+      }
+      getBackgroundImage(BackgroundImageName);
+
+      // 替換氣候詳細資訊
       document.querySelector(".locationName").textContent = ctyName;
       document.querySelector(".PoP12h span").textContent = nowPoP12h + "%";
       document.querySelector(".Wx").textContent = nowWX;
@@ -31,52 +64,30 @@ async function getHeroData(ctyName = "台北") {
 
         let wxBar = wxArr[index].elementValue[1].value;
 
-        // 根據天氣判斷 icon 與背景圖
+        // 根據天氣判斷 icon
         let weatherIconSrc = "";
         let weatherIconAlt = "";
-
-        let BackgroundImageName = "";
-        function getBackgroundImage(BackgroundImageName) {
-          let weatherImage = document.querySelector(".hero-section");
-          let currentBgImage =
-            window.getComputedStyle(weatherImage).backgroundImage;
-          let newBgImage = currentBgImage.replace(
-            /url\([^)]*\)/,
-            `url(/static/src/image/${BackgroundImageName}.jpg)`
-          );
-          weatherImage.style.backgroundImage = newBgImage;
-        }
 
         if (/01/.test(wxBar)) {
           // 晴
           weatherIconSrc = "/static/src/icon/01_clear.svg";
           weatherIconAlt = "Clear weather";
-          BackgroundImageName = "01_clear";
-          getBackgroundImage(BackgroundImageName);
         } else if (/02|03/.test(wxBar)) {
           // 晴時多雲
           weatherIconSrc = "/static/src/icon/03_partly_cloudy_day.svg";
           weatherIconAlt = "Partly-Cloudy weather";
-          BackgroundImageName = "05_cloudy";
-          getBackgroundImage(BackgroundImageName);
         } else if (/0[4-7]|2[4-8]/.test(wxBar)) {
           // 多雲、陰、霧
           weatherIconSrc = "/static/src/icon/05_cloudy.svg";
           weatherIconAlt = "Cloudy weather";
-          BackgroundImageName = "05_cloudy";
-          getBackgroundImage(BackgroundImageName);
         } else if (/0[8-9]|1[0-9]|2[0-2]|29|3[0-9]|41/.test(wxBar)) {
           // 雨
           weatherIconSrc = "/static/src/icon/rainy.svg";
           weatherIconAlt = "rainy weather";
-          BackgroundImageName = "rain";
-          getBackgroundImage(BackgroundImageName);
         } else if (/23|42/.test(wxBar)) {
           // 雪
           weatherIconSrc = "/static/src/icon/15_snowing.svg";
           weatherIconAlt = "snowing weather";
-          BackgroundImageName = "15_snowing";
-          getBackgroundImage(BackgroundImageName);
         }
 
         let forecastItem = document.createElement("div");
