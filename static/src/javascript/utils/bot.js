70af5a3365a978cdfa55ccd488284d18a03610ca
youyuhsuan <stellayou0118@gmail.com>
2024-07-18 21:47:31 +0800

    feat:hero.js,

--- static/src/javascript/utils/bot.js
+++ static/src/javascript/utils/bot.js
@@ -1,19 +1,19 @@
-async function postBotData(ctyName, nowWX, nowWxNum, nowT, nowPoP12h){
-    let data = {
-        location: ctyName,
-        wx: nowWX + "," + nowWxNum,
-        t: nowT,
-        pop12h: nowPoP12h
-      }
-      console.log(data);
+async function postBotData(ctyName, nowWX, nowWxNum, nowT, nowPoP12h) {
+  let data = {
+    location: ctyName,
+    wx: nowWX + "," + nowWxNum,
+    t: nowT,
+    pop12h: nowPoP12h,
+  };
+  console.log(data);
 
-    try{
-        await fetch("/trigger/discord", {
-            method: "POST",
-            headers: {"Content-Type": "application/json"},
-            body: JSON.stringify(data)
-        });
-    }catch(error){
-        console.error(error);
-    }
+  try {
+    await fetch("/trigger/discord", {
+      method: "POST",
+      headers: { "Content-Type": "application/json" },
+      body: JSON.stringify(data),
+    });
+  } catch (error) {
+    console.error(error);
+  }
 }
