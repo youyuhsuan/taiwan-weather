d69363f801fd65cc56efbf7a90a226c5964b397e
arthur3000 <arthur3000.tw@gmail.com>
2024-07-20 00:02:46 +0800

    Modify /model/LocationCache.py, add dataTime for data renew

--- model/LocationCache.py
+++ model/LocationCache.py
@@ -4,20 +4,21 @@ class LocationCache():
     def __init__(self):
         self.data = []
         self.max = 20
-        self.dataTime = datetime.now()
+        self.renewHours = 3
     def getData(self,location):
-        queryTime = self.dataTime.now()
-        difference = queryTime - self.dataTime
-        if (difference.seconds >= 3600 * 3):
-            return None
+        queryTime = datetime.now()
         for i in range(len(self.data)-1,-1,-1):
             if self.data[i]["location"] == location:
+                difference = queryTime - self.data[i]["dataTime"]
+                if (difference.seconds >= 3600 * self.renewHours):
+                    return None
                 found_data = self.data[i]
                 del self.data[i]
                 self.data.append(found_data)
                 return found_data["data"]
         return None
     def setData(self,location,data):
+        dataTime = datetime.now()
         if (len(self.data)>=self.max):
             self.data = self.data[self.max//2:]
-        self.data.append({"location":location, "data":data})
+        self.data.append({"location":location, "data":data, "dataTime":dataTime})
